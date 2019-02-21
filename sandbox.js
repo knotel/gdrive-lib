4f1dd7a067856cf3989fd8ca672d2e3312f39cd5
Caleb Faruki <calebfaruki@gmail.com>
2019-02-20 19:34:12 -0500

    upsert supports rename opt, include IDs, also nonFolderFileCount

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,21 @@
 const GDrive = require('./gdrive')
+const fs = require('fs')
 
 const gdrive = new GDrive()
 gdrive.init()
 
-gdrive.getAll({
-  rootFolderId: '122F51zmThpMDPiyyCLipqboHE6sM_i7s',
-  recursive: true,
-}).then((files) => {
-  console.log(files)
+// gdrive.getAll({
+//   rootFolderId: '1WgUZCgFIJX8P5_V33DoYMGhw4yZiJKga',
+//   recursive: false,
+// }).then((files) => {
+//   console.log(files)
+// })
+
+let schema = JSON.parse(fs.readFileSync('./example.json', 'utf8'))
+
+gdrive.upsert({ rootFolderId: '1RlH4Cx5ElvGsN4j6tbERNImwlxfhbHEU', rename: true }, schema).then(function(folder) {
+  // save space folder contents to JSON with IDs
+  fs.writeFile(`./upsertResponse.json`, JSON.stringify(folder), (err) => {
+    console.log(`Schema San Francisco has been created`)
+  })
 })
